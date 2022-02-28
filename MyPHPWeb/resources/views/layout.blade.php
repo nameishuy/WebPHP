56d7cfeb69cc79f84d95a59adb09a50ba1c719e9
Kiezet2911 <kiezetnguyen@gmail.com>
2022-02-28 08:33:09 +0700

    up

--- MyPHPWeb/resources/views/layout.blade.php
+++ MyPHPWeb/resources/views/layout.blade.php
@@ -9,6 +9,8 @@
 
     <link rel="stylesheet" href="/css/footer.css">
     <link rel="stylesheet" href="/css/header.css">
+    <link rel="stylesheet" href="/css/login.css">
+    <link rel="stylesheet" href="/css/util.css">
 
     <!-- Boostrap 5 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -107,7 +109,9 @@
     </div>
     
     <!-- Script -->
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script src="js/login.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
--- MyPHPWeb/resources/views/signin.blade.php
+++ MyPHPWeb/resources/views/signin.blade.php
@@ -1,5 +1,38 @@
 @extends('layout')
 
 @section('content')
-    <h1>Sign in</h1>
+
+<div class="limiter">
+    <div class="container-login100">
+        <div class="wrap-login100">
+            <form class="login100-form validate-form">
+                <span class="login100-form-title p-b-43">
+                    Login to continue
+                </span>
+
+
+                <div class="wrap-input100 validate-input">
+                    <input class="input100" type="text" name="username" required>
+                    <span class="focus-input100"></span>
+                    <span class="label-input100">UserName</span>
+                </div>
+
+
+                <div class="wrap-input100 validate-input">
+                    <input class="input100" type="password" name="pass" required>
+                    <span class="focus-input100"></span>
+                    <span class="label-input100">Password</span>
+                </div>
+
+                <div class="container-login100-form-btn">
+                    <button class="login100-form-btn">
+                        Login
+                    </button>
+                </div>             
+            </form>
+        </div>
+    </div>
+</div>
+
+
 @endsection
