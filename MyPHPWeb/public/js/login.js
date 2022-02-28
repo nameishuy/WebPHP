c3368fc5174faca359c3ae2286c7f5e3306c9c16
matri001 <63767292+matri001@users.noreply.github.com>
2022-02-28 08:41:05 +0700

    update

--- MyPHPWeb/public/js/login.js
+++ MyPHPWeb/public/js/login.js
@@ -41,30 +41,9 @@
         });
     });
 
-    function validate (input) {
-        if($(input).attr('type') == 'username' || $(input).attr('name') == 'username') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-                return false;
-            }
-        }
-        else {
-            if($(input).val().trim() == ''){
-                return false;
-            }
-        }
-    }
+    
 
-    function showValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).addClass('alert-validate');
-    }
-
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).removeClass('alert-validate');
-    }
+   
     
 
 })(jQuery);
