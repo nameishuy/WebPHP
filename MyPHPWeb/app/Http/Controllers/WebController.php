be3668a8c4f863cdef02e1fdfab6d07278ed0061
Kiezet2911 <kiezetnguyen@gmail.com>
2022-03-13 18:06:49 +0700

    Cập nhật Tìm Kiếm Sách

--- MyPHPWeb/app/Http/Controllers/WebController.php
+++ MyPHPWeb/app/Http/Controllers/WebController.php
@@ -20,35 +20,59 @@ class WebController extends Controller
     }
 
 
-    function product()
+    function product(Request $req)
     {
         $url = "https://bookingapiiiii.herokuapp.com/";
 
-        $chude = json_decode(Http::get($url . 'chude'), true);     
-
+        $chude = json_decode(Http::get($url . 'chude'), true);
+        $search = $req->input('search');
         return view('products', ['chude' => $chude]);
     }
 
-    public static function countbook($MaCD)
+    public static function countbook()
     {
-        $linkbook = null;
-        if ($MaCD == null) {
-            $linkbook = "https://bookingapiiiii.herokuapp.com/sach";
-        } else {
-            $linkbook = "https://bookingapiiiii.herokuapp.com/sachbyCD/" . $MaCD;
+
+        $linkbook = "https://bookingapiiiii.herokuapp.com/sach";
+        if (isset($_GET["chude"])) {
+            $linkbook = "https://bookingapiiiii.herokuapp.com/sachbyCD/" . $_GET["chude"];
+        } elseif (isset($_GET["search"])) {
+            $linkbook = "https://bookingapiiiii.herokuapp.com/sachbyname";
+            $book = json_decode(Http::post($linkbook, ["name" => $_GET["search"]]), true);
+            return count($book);
         }
         $book = json_decode(Http::get($linkbook), true);
         return count($book);
     }
 
-    public static function getlist($link, $pages, $last)
+    public static function getlist($pages, $last)
     {
-        $url = "https://bookingapiiiii.herokuapp.com/" . $link . "/" . $pages . "/" . $last;
-        $book = json_decode(Http::get($url), true);     
+        $url = "https://bookingapiiiii.herokuapp.com/sachpagination/" . $pages . "/" . $last;
+        //
+        if (isset($_GET["chude"])) {
+            //
+            $url = "https://bookingapiiiii.herokuapp.com/sachpaginationbychude/" . $_GET["chude"] . "/" . $pages . "/" . $last;
+
+            //
+        } elseif (isset($_GET["search"])) {
+            //
+            $url = "https://bookingapiiiii.herokuapp.com/sachpaginationSearch";
+            //
+            $book = json_decode(Http::post($url, [
+                "keyword" => $_GET["search"],
+                "page" => $pages,
+                "limit" => $last
+            ]), true);
+            //
+            return $book;
+        }
+        //
+        $book = json_decode(Http::get($url), true);
+        //      
         return $book;
     }
 
-    public static function ifEmptyCart(){
+    public static function ifEmptyCart()
+    {
         return true;
     }
 }
