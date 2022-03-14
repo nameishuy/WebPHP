431de208b4d6de1eee3b1161056db69d93d5349c
Yuhtaro <85222053+nameishuy@users.noreply.github.com>
2022-03-14 18:19:47 +0700

    Call API for Account Manager

--- MyPHPWeb/app/Http/Controllers/AdminController.php
+++ MyPHPWeb/app/Http/Controllers/AdminController.php
@@ -12,6 +12,7 @@ class AdminController extends Controller
 
     function AdminIndex(Request $req)
     {
+
         if (isset($req->session()->get("UserLogin")["Role"])) {
             if($req->session()->get("UserLogin")["Role"] == true){
                 return view('admin_index');
@@ -41,7 +42,20 @@ class AdminController extends Controller
     {
         if (isset($req->session()->get("UserLogin")["Role"])) {
             if($req->session()->get("UserLogin")["Role"] == true){
-                return view('admin_account');
+                
+                $url = "https://bookingapiiiii.herokuapp.com/";
+                $role = "false";
+                $listUser = json_decode(Http::get($url . 'khachhangforadmin/'. $role),true);
+                return view('admin_account', ['listUser'=> $listUser]);
+            }else return view('admin_notAdmin');
+        }else return view('admin_notAdmin');
+    }
+
+    function AdminBill(Request $req)
+    {
+        if (isset($req->session()->get("UserLogin")["Role"])) {
+            if($req->session()->get("UserLogin")["Role"] == true){
+                return view('admin_billpay');
             }else return view('admin_notAdmin');
         }else return view('admin_notAdmin');
     }
--- MyPHPWeb/resources/views/admin_account.blade.php
+++ MyPHPWeb/resources/views/admin_account.blade.php
@@ -12,21 +12,35 @@
         <div class="Title__setting">Thiết Lập</div>
     </div>
     <div class="Admin__Account-Body">
-        <div class="Admin__Account-Account-Details">
-            <div class="Checkbox__Account">
-                <input type="checkbox" name="" class="checkbox" id="checkbox__account">
-            </div>
-
-            <div class="User__username">Tài Khoản</div>
-            <div class="User__email" >Email</div>
-            <div class="User__role">Admin</div>
-
-            <div class="User__setting">
-                <div class="User__setting-deleteAccount" onclick="showDialogDeleteAccount()">
-                    Xóa Tài Khoản
+        @foreach ($listUser as $user)
+        <div class="Admin__Account-Account-Details">             
+                <div class="Checkbox__Account">
+                    <input type="checkbox" name="" class="checkbox" id="checkbox__account">
                 </div>
-            </div>
+
+                <div class="User__username">{{$user['Taikhoan']}}</div>
+
+                <?php 
+                    if(isset($user['Email'])) echo "<div class='User__email' >{$user['Email']}</div>";
+                    else echo "<div class='User__email' >Chưa Cập Nhật</div>";
+                ?>
+
+                <?php 
+                    if($user['Role']==true) echo "<div class='User__role' >Admin</div>";
+                    else {
+                        echo "<div class='User__role' >Khách</div>";
+                        echo "
+                            <div class='User__setting'>
+                                <div class='User__setting-deleteAccount' onclick='showDialogDeleteAccount()'>
+                                    Xóa Tài Khoản
+                                </div>
+                            </div>
+                        ";
+                    } 
+                ?>
+
         </div>
+        @endforeach
         <div class="UpdateAll__Setting">
             Cấp Quyền Admin
         </div>
